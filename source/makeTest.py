5a9084db4b30dc3e33c4d309a357af1881b25f9b
deprekate <deprekate@gmail.com>
2019-08-26 15:59:52 -0700

    Fixed code and added make_train_set

--- source/makeTest.py
+++ source/makeTest.py
@@ -24,7 +24,8 @@ class ShannonScore:
 
     def reset(self):
         self.total = 0
-        self._values = array.array('i', '\x00' * self._values.itemsize * len(self._values))
+        #self._values = array.array('i', '\x00' * self._values.itemsize * len(self._values))
+        self._values = array.array('i', [0] * len(self._values))
 
     def addValue(self, seq):
         mer = 12
@@ -47,7 +48,6 @@ class ShannonScore:
             if (p > 0):
                 H = H + p * (math.log(p) / math.log(2))
                 found_total = found_total + self._values[self._key_to_index[i]]
-
         H = -H
         if H <= 0:
             return 0
@@ -55,14 +55,12 @@ class ShannonScore:
         myslope = freq_found / H
         return myslope
 
-
 def read_contig(organismPath):
     try:
         f_dna = open(organismPath + '/contigs', 'r')
     except:
         print('cant open contig file ', organismPath)
         return ''
-
     dna = {}
     seq = ''
     name = ''
@@ -85,11 +83,9 @@ def read_contig(organismPath):
             seq = ''
         else:
             seq = seq + i.strip()
-
     dna[name] = seq
     f_dna.close()
     return dna
-
 
 def my_sort(orf_list):
     n = len(orf_list)
@@ -119,7 +115,6 @@ def my_sort(orf_list):
                 else:
                     if orf_list[i]['stop'] > orf_list[j]['start']:
                         flag = 1
-
             # swap
             if flag == 1:
                 temp = orf_list[i]
@@ -129,9 +124,11 @@ def my_sort(orf_list):
         i += 1
     return orf_list
 
-
 def complement(gene):
-    complements = string.maketrans('acgtrymkbdhvACGTRYMKBDHV', 'tgcayrkmvhdbTGCAYRKMVHDB')
+    try:
+        complements = string.maketrans('acgtrymkbdhvACGTRYMKBDHV', 'tgcayrkmvhdbTGCAYRKMVHDB')
+    except:
+        complements = ''.maketrans('acgtrymkbdhvACGTRYMKBDHV', 'tgcayrkmvhdbTGCAYRKMVHDB')
     rcseq = gene.translate(complements)[::-1]
     return rcseq
 
@@ -141,15 +138,13 @@ def find_all_median(x):
         all_len.append((abs(x[i]['start'] - x[i]['stop'])) + 1)
     return find_median(all_len)
 
-
 def find_median(all_len):
-    n = len(all_len) / 2
+    n = int(round(len(all_len) / 2))
     all_len.sort()
     if len(all_len) == n * 2:
         return (all_len[n] + all_len[n - 1]) / float(2)
     else:
         return all_len[n]
-
 
 def find_avg_length(orf_list):
     x = []
@@ -157,12 +152,10 @@ def find_avg_length(orf_list):
         x.append(abs(orf_list[i]['start'] - orf_list[i]['stop']))
     return sum(x) / len(x)
 
-
 def find_atgc_skew(seq):
     seq = seq.upper()
     total_at = 0.0
     total_gc = 0.0
-
     #          A    G     C   T
     scores = {
         'A': [1.0, 0.0, 0.0, 0.0],
@@ -181,16 +174,13 @@ def find_atgc_skew(seq):
         'V': [0.3, 0.3, 0.3, 0.0], #acg
         'N': [0.25, 0.25, 0.25, 0.25], #acgt
     }
-
     counts = [0, 0, 0, 0]
-
     for base in seq:
         if base not in scores:
             sys.stderr.write("ERROR: found base " + base + " that is not in the iupac code. Skipped\n")
             continue
         for i,j in enumerate(scores[base]):
             counts[i] += j
-
     total_at = counts[0] + counts[3]
     total_gc = counts[1] + counts[2]
     if (total_at * total_gc) == 0:
@@ -241,9 +231,7 @@ def make_set_test(organismPath, output_dir, window, INSTALLATION_DIR):
         infile = open(organismPath + '/Features/peg/tbl', 'r')
     except:
         sys.exit('ERROR: Cannot open file ' + organismPath + '/Features/peg/tbl')
-
     dna = read_contig(organismPath)
-
     # open host/bact dna file which has a contig
     for line in infile:
         temp = re.split('\t', line.strip())
@@ -251,30 +239,24 @@ def make_set_test(organismPath, output_dir, window, INSTALLATION_DIR):
             ttemp = re.split(',', temp[1])
             temp[1] = ttemp[len(ttemp) - 1]
         temp1 = re.split('_', temp[1])
-
         contig = temp[1][:temp[1][:temp[1].rfind('_')].rfind('_')]
-
         start = int(temp1[len(temp1) - 2])
         stop = int(temp1[len(temp1) - 1])
-
         # save info for sorting orf
         if contig in all_orf_list:
             x = len(all_orf_list[contig])
         else:
             x = 0
             all_orf_list[contig] = {}
-
         all_orf_list[contig][x] = {}
         all_orf_list[contig][x]['start'] = start
         all_orf_list[contig][x]['stop'] = stop
         all_orf_list[contig][x]['peg'] = temp[0]
     infile.close()
-
     try:
         outfile = open(output_dir + 'testSet.txt', 'w')
     except:
         sys.exit('ERROR: Cannot open file for writing: testSet.txt')
-
     outfile.write('orf_length_med\tshannon_slope\tat_skew\tgc_skew\tmax_direction\n')
     for mycontig in all_orf_list:
         orf_list = my_sort(all_orf_list[mycontig])
@@ -294,13 +276,11 @@ def make_set_test(organismPath, output_dir, window, INSTALLATION_DIR):
             t_skew = []
             g_skew = []
             c_skew = []
-
             for j in range(i - int(window / 2), i + int(window / 2)):
                 if ((j < 0) or (j >= len(orf_list))):
                     continue
                 start = orf_list[j]['start']
                 stop = orf_list[j]['stop']
-
                 if start < stop:
                     bact = dna[mycontig][start - 1:stop]
                     direction.append(1)  # direction
@@ -309,73 +289,68 @@ def make_set_test(organismPath, output_dir, window, INSTALLATION_DIR):
                     bact = bact[::-1]
                     bact = complement(bact)
                     direction.append(-1)  # direction
-
                 if len(bact) < 3:
                     print('Short Protein Found!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
                     j += 1
                     continue
-
                     # at skew
                 xa, xt, xg, xc = find_atgc_skew(bact)
                 a_skew.append(xa)
                 t_skew.append(xt)
                 g_skew.append(xg)
                 c_skew.append(xc)
-
                 # length
                 length.append(len(bact))
-
                 # shannon
                 my_shannon_scores.addValue(bact)
                 j += 1
-
-                # write in file for one window
-                mylength = find_median(length) - all_median  # find_mean(length)
-                fileWriteStr = ''
-                fileWriteStr += str(mylength) + '\t'
-                fileWriteStr += str(my_shannon_scores.getSlope()) + '\t'
-                a = sum(a_skew) / len(a_skew)
-                t = sum(t_skew) / len(t_skew)
-                c = sum(c_skew) / len(c_skew)
-                g = sum(g_skew) / len(g_skew)
-                at = math.fabs(a - t) / avg_at_skew if avg_at_skew else 0
-                gc = math.fabs(g - c) / avg_gc_skew if avg_gc_skew else 0
-                fileWriteStr += str(at) + '\t'
-                fileWriteStr += str(gc) + '\t'
-
-                # orf direction
-                orf = []
-                x = 0
-                flag = 0
-                for ii in direction:
-                    if (ii == 1):
-                        if (flag == 0):
-                            x += 1
-                        else:
-                            orf.append(x)
-                            x = 1
-                            flag = 0
+            # write in file for one window
+            mylength = find_median(length) - all_median  # find_mean(length)
+            outfile.write(str(mylength))
+            outfile.write('\t')
+            outfile.write(str(my_shannon_scores.getSlope()))
+            outfile.write('\t')
+            a = sum(a_skew) / len(a_skew)
+            t = sum(t_skew) / len(t_skew)
+            c = sum(c_skew) / len(c_skew)
+            g = sum(g_skew) / len(g_skew)
+            at = math.fabs(a - t) / avg_at_skew if avg_at_skew else 0
+            gc = math.fabs(g - c) / avg_gc_skew if avg_gc_skew else 0
+            outfile.write(str(at))
+            outfile.write('\t')
+            outfile.write(str(gc))
+            outfile.write('\t')
+            # orf direction
+            orf = []
+            x = 0
+            flag = 0
+            for ii in direction:
+                if (ii == 1):
+                    if (flag == 0):
+                        x += 1
                     else:
-                        if (flag == 1):
-                            x += 1
-                        else:
-                            if (flag < 1 and x > 0):
-                                orf.append(x)
-                            x = 1
-                            flag = 1
-                orf.append(x)
-                orf.sort()
-                if len(orf) == 1:
-                    fileWriteStr += str(orf[len(orf) - 1]) + '\n'
+                        orf.append(x)
+                        x = 1
+                        flag = 0
                 else:
-                    fileWriteStr += str(orf[len(orf) - 1] + orf[len(orf) - 2]) + '\n'
-                outfile.write(fileWriteStr)
-                i += 1
+                    if (flag == 1):
+                        x += 1
+                    else:
+                        if (flag < 1 and x > 0):
+                            orf.append(x)
+                        x = 1
+                        flag = 1
+            orf.append(x)
+            orf.sort()
+            if len(orf) == 1:
+                outfile.write(str(orf[len(orf)-1]))
+            else:
+                outfile.write(str(orf[len(orf)-1]+orf[len(orf)-2]))
+            outfile.write('\n')
+            i += 1
     outfile.close()
 
-
 ##################### function call #################################
-
 
 def call_make_test_set(organismPath, output_dir, INSTALLATION_DIR):
     window = 40
