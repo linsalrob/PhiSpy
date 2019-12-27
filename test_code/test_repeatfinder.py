8e3c8ddc35bb46b129eea3db86ee10c909d74ec5
Robert Edwards <raedwards@gmail.com>
2019-12-26 16:01:11 -0800

    test code for the new repeatfinder

---
+++ test_code/test_repeatfinder.py
@@ -0,0 +1,25 @@
+"""
+Test the implementation of the repeatfinder extension
+"""
+
+import os
+import sys
+import argparse
+
+from roblib import bcolors
+
+import PhiSpyRepeatFinder
+import pprint
+
+s = "TTTTTTTTTTTTagcaTTTTTTTTTTTT"
+print(f"s: {s}")
+r = PhiSpyRepeatFinder.repeatFinder(s, 0)
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(r)
+
+with open("test.fna", 'r') as f:
+    seqid = f.readline().strip()
+    fna = f.readline().strip()
+    r = PhiSpyRepeatFinder.repeatFinder(fna, 0)
+    pp.pprint(r)
+
