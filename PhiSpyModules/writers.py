211b84104c2a4d482bc28525acd55aeb882e291d
Robert Edwards <raedwards@gmail.com>
2020-05-20 17:05:44 -0700

    Mostly minor changes, but a small rewrite of the write_phage_and_bact function

--- PhiSpyModules/writers.py
+++ PhiSpyModules/writers.py
@@ -14,79 +14,79 @@ import PhiSpyModules.version as version
 __author__ = 'Rob Edwards'
 
 
-def write_gff3(output_dir, pp, verbose=False):
+def write_gff3(output_dir, pp):
     """
     Write GFF3 code. This was adapted from code contribued by [Jose Francisco Sanchez-Herrero]
     (https://github.com/JFsanchezherrero/)
 
     :param output_dir: the location to write to
     :param pp: the list of prophage objects
-    :param verbose: more output
     """
 
-    ## GFF output
-    out_GFF = open(os.path.join(output_dir, "prophage.gff3"), "w")
-    out_GFF.write("##gff-version 3")
-    out_GFF.write("\n")
+    # GFF output
+    out_gff = open(os.path.join(output_dir, "prophage.gff3"), "w")
+    out_gff.write("##gff-version 3")
+    out_gff.write("\n")
     
-    ## loop through pp results
+    # loop through pp results
     for i in pp:
-        ## list
-        list_write = ["pp" + str(i), str(pp[i]['contig']), str(pp[i]['start']), str(pp[i]['stop'])]
-
-        ## GFF  
-        ## strand is not known...
-        out_GFF.write(str(pp[i]['contig']) + 
+        # GFF
+        # strand is not known...
+        out_gff.write(str(pp[i]['contig']) +
                       '\tPhiSpy\tprophage_region\t' +
                       str(pp[i]['start']) + '\t' + 
                       str(pp[i]['stop']) + '\t.\t.' + 
                       ' \t.\tID=pp' + str(i))
-        out_GFF.write('\n')
+        out_gff.write('\n')
 
-        ## is there att sequence?
         if 'att' not in pp[i]:
-            pp[i]['att']=""
+            pp[i]['att'] = ""
         else:
-            ## attL 
-            out_GFF.write(str(pp[i]['contig']) +
+            # attL
+            out_gff.write(str(pp[i]['contig']) +
                           '\tPhiSpy\tattL\t' +
                           str(pp[i]['att'][0]) + '\t' + 
                           str(pp[i]['att'][1]) + '\t.\t.' + 
                           ' \t.\tID=pp' + str(i))
-            out_GFF.write('\n')
+            out_gff.write('\n')
 
-            ## attR 
-            out_GFF.write(str(pp[i]['contig']) +
+            # attR
+            out_gff.write(str(pp[i]['contig']) +
                           '\tPhiSpy\tattR\t' +
                           str(pp[i]['att'][2]) + '\t' +
                           str(pp[i]['att'][3]) + '\t.\t.' +
                           ' \t.\tID=pp' + str(i))
-            out_GFF.write('\n')
+            out_gff.write('\n')
         
-    out_GFF.close()
+    out_gff.close()
+
 
 def write_genbank(infile, record, output_directory, pp):
     """
     Write prophages and their potential attachment sites in updated input GenBank file.
     :param infile: path to input file
     :param record: SeqRecord generator of input file
-    :param output_dir: the location to write to
+    :param output_directory: the location to write to
     :param pp: the list of prophage objects
     """
 
-    prophage_feature_type = 'misc_feature' # / prophage_region
+    prophage_feature_type = 'misc_feature'  # / prophage_region
     outfile = os.path.join(output_directory, os.path.basename(infile))
     for i in pp:
         record.get_entry(pp[i]['contig']).append_feature(SeqFeature(
-                    location = FeatureLocation(pp[i]['start'], pp[i]['stop']),
-                    type = prophage_feature_type,
-                    strand = 1,
-                    qualifiers = OrderedDict({'note': f'prophage region pp{i} identified with PhiSpy v{version.__version__}'})))
+                    location=FeatureLocation(pp[i]['start'], pp[i]['stop']),
+                    type=prophage_feature_type,
+                    strand=1,
+                    qualifiers=OrderedDict(
+                        {'note': f'prophage region pp{i} identified with PhiSpy v{version.__version__}'}
+                    )))
+
         record.get_entry(pp[i]['contig']).append_feature(SeqFeature(
-                    location = FeatureLocation(int(pp[i]['att'][0]), int(pp[i]['att'][1])) + FeatureLocation(int(pp[i]['att'][2]), int(pp[i]['att'][3])),
-                    type = 'repeat_region',
-                    strand = 1,
-                    qualifiers = OrderedDict({'note': f'prophage region pp{i} potential attachment sites'})))
+                    location=FeatureLocation(int(pp[i]['att'][0]), int(pp[i]['att'][1])) +
+                             FeatureLocation(int(pp[i]['att'][2]), int(pp[i]['att'][3])),
+                    type='repeat_region',
+                    strand=1,
+                    qualifiers=OrderedDict({'note': f'prophage region pp{i} potential attachment sites'})))
 
     SeqIO.write(record, outfile, 'genbank')
 
@@ -100,12 +100,9 @@ def write_phage_and_bact(output_dir, pp, dna):
     contig_to_phage = {}
     for i in pp:
         contig = pp[i]['contig']  # contig name
-        # sys.stderr.write(contig)
         if contig not in contig_to_phage:
             contig_to_phage[contig] = set()
         contig_to_phage[contig].add(i)
-    # sys.stderr.write('dictionary of contigs with phages')
-    # sys.stderr.write(contig_to_phage)
 
     for contig in dna:
         if contig not in contig_to_phage:
@@ -114,6 +111,7 @@ def write_phage_and_bact(output_dir, pp, dna):
         pps1 = list(contig_to_phage[contig])
         pps = sorted(pps1, key=lambda k: pp[k]['start'])
         bactstart = 0
+        dnaseq = ""
         for ppnum in pps:
             pphagestart = pp[ppnum]['start']
             pphagestop = pp[ppnum]['stop']
@@ -121,13 +119,11 @@ def write_phage_and_bact(output_dir, pp, dna):
             phageseq = dna[contig][pphagestart:pphagestop]
             phage_out.write(f">{contig}_{pphagestart}_{pphagestop} [pp {ppnum}]\n{phageseq}\n")
 
-            dnaseq = dna[contig][bactstart:pphagestart - 1]
-            nphageseq = str.replace(phageseq, 'A', 'N').replace('T', 'N').replace('C', 'N').replace('G', 'N')
-            bacteria_out.write(f">{contig}_{i}_{pphagestart}\n{dnaseq}{nphageseq}\n")
-
+            dnaseq += dna[contig][bactstart:pphagestart - 1]
+            dnaseq += "N" * len(phageseq)
             bactstart = pphagestop + 1
-        dnaseq = dna[contig][bactstart:]
-        bacteria_out.write(f">{contig}_{i}_{len(dna[contig])}\n{dnaseq}\n")
+        dnaseq += dna[contig][bactstart:]
+        bacteria_out.write(f">{contig} [phage regions replaced with N]\n{dnaseq}\n")
 
     phage_out.close()
     bacteria_out.close()
