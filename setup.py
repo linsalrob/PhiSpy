fc0e99237832f96ec50b7b52bc1d051b13b3f632
Rob Edwards <raedwards@gmail.com>
2021-05-17 21:12:36 +0930

    Fixing setup vs. requirements. See https://caremad.io/posts/2013/07/setup-vs-requirement/ for more information on why

--- setup.py
+++ setup.py
@@ -58,8 +58,8 @@ def main():
         install_requires = [
             'biopython >= 1.74',
             'numpy >= 1.16.0',
-            'scikit - learn >= 0.21.3',
-            'bcbio - gff >= 0.6.6'
+            'scikit-learn >= 0.21.3',
+            'bcbio-gff >= 0.6.6'
         ]
         )
 
